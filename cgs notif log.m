cad8c0d388a0e581020d11dabe82cede788941c8
Amy <57969415+ASentientBot@users.noreply.github.com>
2022-05-13 00:49:24 -0400

    significantly improve SLS notification logger

--- cgs notif log.m
+++ cgs notif log.m
@@ -1,17 +1,33 @@
-// clang -fmodules -I ../*/non-metal-frameworks/SkyLight -I ../*/non-metal-common/Utils -dynamiclib 'cgs notif log.m' -o /tmp/c -F /S*/L*/PrivateFrameworks -framework SkyLight && codesign -f -s - /tmp/c && launchctl setenv DYLD_INSERT_LIBRARIES /tmp/c && log stream --predicate 'message contains "fake_SLSRegisterConnectionNotifyProc"'
+// clang -fmodules -I ../non-metal-frameworks/SkyLight -I ../non-metal-common/Utils -dynamiclib 'cgs notif log.m' -o /tmp/c -F /S*/L*/PrivateFrameworks -framework SkyLight && codesign -f -s - /tmp/c && launchctl setenv DYLD_INSERT_LIBRARIES /tmp/c && log stream --predicate 'message contains "Moraea: notif"'
 
 @import AppKit;
 #import "Utils.h"
 #import "Extern.h"
 
-unsigned int fake_SLSRegisterConnectionNotifyProc(unsigned int edi_connectionID,void (*rdi_callback)(),unsigned int edx_type,char* rcx_context)
+char* (*stringForType)(int);
+
+__attribute__((constructor)) void load()
 {
 	traceLog=true;
 	tracePrint=false;
+
+	long base=SLSMainConnectionID-0x1d8322;
+	stringForType=base+0x125639;
+}
+
+unsigned int fake_SLSRegisterConnectionNotifyProc(unsigned int edi_connectionID,void (*rsi_callback)(),unsigned int edx_type,char* rcx_context)
+{
+	trace(@"notif %x %s %@",edx_type,stringForType(edx_type),NSThread.callStackSymbols);
 	
-	trace(@"fake_SLSRegisterConnectionNotifyProc %d %p 0x%x %p",edi_connectionID,rdi_callback,edx_type,rcx_context);
+	return SLSRegisterConnectionNotifyProc(edi_connectionID,rsi_callback,edx_type,rcx_context);
+}
+
+unsigned int fake_SLSRegisterNotifyProc(void (*rdi_callback)(),unsigned int esi_type,char* rdx_context)
+{
+	trace(@"notif %x %s %@",esi_type,stringForType(esi_type),NSThread.callStackSymbols);
 	
-	return SLSRegisterConnectionNotifyProc(edi_connectionID,rdi_callback,edx_type,rcx_context);
+	return SLSRegisterNotifyProc(rdi_callback,esi_type,rdx_context);
 }
 
 DYLD_INTERPOSE(fake_SLSRegisterConnectionNotifyProc,SLSRegisterConnectionNotifyProc)
+DYLD_INTERPOSE(fake_SLSRegisterNotifyProc,SLSRegisterNotifyProc)
